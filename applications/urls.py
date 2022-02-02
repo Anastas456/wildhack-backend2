8624db15aceadf9fa4758afec8c9d854e9eca22b
Anastas456 <wild456west@gmail.com>
2022-02-02 19:29:38 +0300

    view для обработки формы

--- applications/urls.py
+++ applications/urls.py
@@ -1,8 +1,16 @@
 from rest_framework import routers
 from .api import ApplicationsViewSet
+# from django.conf.urls import url
+from django.urls.conf import include, path, re_path
+from .views import volunteer_application_form
 
 
 router = routers.DefaultRouter()
 router.register('api/applications', ApplicationsViewSet, 'applications')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls))
+]
+urlpatterns+=[
+    re_path(r'^api/applications$', volunteer_application_form)
+]
--- applications/views.py
+++ applications/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.parsers import JSONParser
+from .serializers import ApplicationsSerializer
+from django.http.response import JsonResponse
+from rest_framework import status
 # Create your views here.
+
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def volunteer_application_form(request):
+
+    form_data = JSONParser().parse(request)
+    form_serializer = ApplicationsSerializer(data=form_data)
+    if form_serializer.is_valid():
+        form_serializer.save()
+        return JsonResponse(form_serializer.data, status=status.HTTP_201_CREATED) 
+    return JsonResponse(form_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
